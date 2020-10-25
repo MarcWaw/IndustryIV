2c146886975ac51f1b29d32242bb8c6c48d5ca09
Marcin Wawszczak <235274@student.pwr.edu.pl>
2020-10-25 11:32:19 +0100

    Style

--- index.php
+++ index.php
@@ -2,16 +2,40 @@
 <html lang="pl">
 <head>
     <meta charset="utf-8"/>
-    <title>VRP – problem marszrutyzacji pojazdów</title>
+    <title>Problem marszrutyzacji pojazdów</title>
+    <link rel="stylesheet" href="Styles/index_style.css" type="text/css"/>
+    
 </head>
 <body>
-<h1>VRP – problem marszrutyzacji pojazdów</h1>
-<form action="load_file.php" method="post">
-Nazwa Pliku:
-<input type="text" name="file_name"/> 
-<input type="submit" value="Wczytaj" />
-<br />
-</form>
+    <div id="container">
+        <div id="logo">
+            <h1>VRP – problem marszrutyzacji pojazdów</h1>
+        </div>
+        <div id="nav">
+            Home <br/>
+            Update <br/>
+            Table <br/>
+        </div>
+        <div id="content">
+            <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna 
+                 aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                 ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                 Duis aute irure dolor in reprehenderit in voluptate velit 
+                 esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+                 occaecat cupidatat non proident, sunt in culpa qui officia 
+                 deserunt mollit anim id est laborum.</h2>
+        </div>
+        <div id="footer">
+            Laboratorium Przemysł 4.0 
+        </div>
+    </div>
 
+    <form action="load_file.php" method="post">
+        Nazwa Pliku:
+        <input type="text" name="file_name"/> 
+        <input type="submit" value="Wczytaj" />
+        <br />
+    </form>
 </body>
 </html>
